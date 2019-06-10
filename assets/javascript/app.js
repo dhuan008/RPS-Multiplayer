f9069992f58e45630c4517ca5299245fdb5a2154
David Huang <daveshuang@gmail.com>
2019-06-09 18:10:58 -0700

    added firebase initializers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,31 @@
+// Game Object
+var rpsM = {
+    // ========================
+    // Variables 
+    // ========================
+
+
+    // ========================
+    // Methods 
+    // ========================
+
+    // Firebase config and initialization details
+    initFirebase: function () {
+        // RPS-Multiplayer's Firebase configuration
+        var firebaseConfig = {
+            apiKey: "AIzaSyC6tUCxvDD9K69cFl65qzNUzd7Wk7ZrKMY",
+            authDomain: "rps-multiplayer-1d946.firebaseapp.com",
+            databaseURL: "https://rps-multiplayer-1d946.firebaseio.com",
+            storageBucket: "rps-multiplayer-1d946.appspot.com",
+        };
+        // Initialize Firebase
+        firebase.initializeApp(firebaseConfig);
+    }
+};
+
+
 // Shorthand for $( document ).ready()
 $(function () {
-    console.log("ready!");
+    rpsM.initFirebase();
+    console.log("Ready!");
 });
